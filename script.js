2505b1beea723ff97900970710bbf2027ba482a0
Yafei Liu <yafeiliu@yafeis-air.myfiosgateway.com>
2022-05-19 15:55:20 -0400

    Add localstorage to set and get item

--- script.js
+++ script.js
@@ -3,5 +3,24 @@ $("#currentDay").text(moment().format("dddd,MMM,Do"));
 $(".saveBtn").on("click", function () {
   var text = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
-  localStorage.setItem(text, time);
+  localStorage.setItem(time, text);
 });
+
+var currentHour = moment().hour();
+
+for (i = 0; i < 10; i++) {
+  $(".container")
+    .children()
+    .eq(i)
+    .children()
+    .eq(1)
+    .val(localStorage.getItem(i + 8));
+
+  if (currentHour < i + 8) {
+    $(".container").children().eq(i).children().eq(1).addClass("future");
+  } else if (currentHour == i + 8)
+    $(".container").children().eq(i).children().eq(1).addClass("present");
+  else {
+    $(".container").children().eq(i).children().eq(1).addClass("past");
+  }
+}
